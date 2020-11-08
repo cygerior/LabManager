a84043ffad171fb7d33b31903b80b3082fc522ec
Regis ROY <regis.roy@free.fr>
2020-11-08 18:46:52 +0100

    added preliminary IP import

--- IPLabo/admin.py
+++ IPLabo/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from import_export import resources
+
 from .models import *
 
 # Register your models here.
 
-admin.site.register(IpPool)
+from import_export.admin import ImportExportModelAdmin
+
+
+class IpResource(resources.ModelResource):
+
+    class Meta:
+        model = IpPool
+
+
+class IpAdmin(ImportExportModelAdmin):
+    resource_class = IpResource
+
+
+admin.site.register(IpPool, IpAdmin)
