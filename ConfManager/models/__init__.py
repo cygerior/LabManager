b0ac6a3c38a1f71a145475762d835a22e34a53fd
cygerior <cygerior@gmail.com>
2021-07-13 16:47:45 +0200

    Move Configuration to configuration.py

--- ConfManager/models/__init__.py
+++ ConfManager/models/__init__.py
@@ -1,8 +1,9 @@
 from .board import BoardType, Board
+from .configuration import Configuration
 from .board_definition import BoardDefinition
 from .device import Device
 from .interfaces import Interface
 from .module_definition import ModuleDefinition
 from .power import PowerController, PowerSupply
 from .rack import RackSlot, Rack
-from .reservation import Configuration, Reservation, Resource, Label
+from .reservation import Reservation, Resource, Label
---
+++ ConfManager/models/configuration.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Configuration(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(null=True)
+    board = models.ForeignKey('Board', on_delete=models.SET_NULL, null=True)
+    power = models.ForeignKey('PowerSupply', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
