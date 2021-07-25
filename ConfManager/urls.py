840e3ac632c34160121b62b529cae23bb4fc9ba1
cygerior <cygerior@gmail.com>
2021-07-25 15:54:23 +0200

    Using url in index.html

--- ConfManager/urls.py
+++ ConfManager/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from ConfManager import views
 
+app_name = 'conf_manager'
 urlpatterns = [
-    path('home', views.home),
+    path('home', views.home, name='index'),
     path('config/<int:id_config>', views.view_config),
     path('date', views.date),
     path('add_rpc/<uri>/<int:out_count>', views.add_rpc),
--- LabNet/urls.py
+++ LabNet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lab_net'
 urlpatterns = [
     path('', views.list, name='index'),
     path('add_pool/<ip_start>/<ip_end>/', views.add_range)
