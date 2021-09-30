c6c9fd8957503048eecc84df4ab7309e77f9c0f2
Regis ROY <regis.roy@free.fr>
2021-09-30 22:51:59 +0200

    remove name from Device

--- ConfManager/models/device.py
+++ ConfManager/models/device.py
@@ -8,18 +8,17 @@ class DeviceType(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Device(models.Model):
-    name = models.CharField(max_length=30)
     board = models.ForeignKey("Board", on_delete=models.CASCADE)
     interfaces = models.ManyToManyField('Interface')
     type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["name", "board_id"], name="unique_target_device")
+            models.UniqueConstraint(fields=["board_id", "type_id"], name="unique_target_device")
         ]
 
     def __str__(self):
-        return f'{self.board}-{self.name}'
+        return f'{self.board}-{self.type}'
